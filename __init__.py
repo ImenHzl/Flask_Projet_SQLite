d4838e5b01d0c434c48ef72729b4e9e4982c3198
ImenHzl <166798845+ImenHzl@users.noreply.github.com>
2024-05-23 15:43:42 +0200

    Update __init__.py with authentification

--- __init__.py
+++ __init__.py
@@ -79,13 +79,15 @@ def enregistrer_client():
 
 @app.route('/fiche_nom/<string:nom>')
 def Nomfiche(nom):
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom,))
-    data = cursor.fetchall()
-    conn.close()
-    # Rendre le template HTML et transmettre les données
-    return render_template('read_nom.html', data=data)
+    if request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
+            session['authentifie'] = True
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM clients WHERE nom = ?', (nom,))
+            data = cursor.fetchall()
+            conn.close()
+            # Rendre le template HTML et transmettre les données
+            return render_template('read_nom.html', data=data)
                                                                                                                                   
 if __name__ == "__main__":
   app.run(debug=True)
